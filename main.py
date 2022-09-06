7f2ddf339322e8fcb252e5bc3bf16b508cb14b35
ParachuteMountain <106806611+ParachuteMountain@users.noreply.github.com>
2022-09-06 18:11:17 +0530

    0o0o0o0o

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {"HOME": "Home v2"}
+    return jsonify(summary = {"HOME": "Home v2"})
 
 @app.route('/v0.5/patients/on-find', methods=['POST'])
 def pat_on_find():
     print("Patient find received!")
     # print(request.json)
-    return {"Patient Find Response": request.json}
+    return jsonify(summary = {"Patient Find Response": request.json})
 
 @app.route('/v0.5/consent-requests/on-init', methods=['POST'])
 def con_req_on_init():
     print("Con Req on init received!")
     # print(request.json)
-    return {"Con Req on init Response": request.json}
+    return jsonify(summary = {"Con Req on init Response": request.json})
 
 if __name__ == '__main__':
     app.run(debug=False)
