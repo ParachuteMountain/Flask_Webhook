4e605c2b37d6698a2346c651b5281884ac5c15e3
ParachuteMountain <106806611+ParachuteMountain@users.noreply.github.com>
2022-09-06 18:02:53 +0530

    -p-p-p-p

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print("Home v1")
-    return {"home":"home v1"}
+    return {"HOME": "Home v2"}
+
+@app.route('/v0.5/patients/on-find', methods=['POST'])
+def pat_on_find():
+    print("Patient find received!")
+    # print(request.json)
+    return {"Patient Find Response": request.json}
 
 @app.route('/v0.5/consent-requests/on-init', methods=['POST'])
-def respond():
-    print("On_Init Recived!")
-    print(request.json)
-    return Response(status=200)
+def con_req_on_init():
+    print("Con Req on init received!")
+    # print(request.json)
+    return {"Con Req on init Response": request.json}
 
 if __name__ == '__main__':
     app.run(debug=False)
