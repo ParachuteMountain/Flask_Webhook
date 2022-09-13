93273b8d500fdf56b235c3af02fc2b5d83690548
Abhishek Patil <abhishek.patil@paramountinsuretech.com>
2022-09-13 17:59:42 +0530

    VS upd1

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(summary = {"HOME": "Home v2"})
+    return jsonify(summary = {"Home": "Home v2"})
 
 @app.route('/v0.5/patients/on-find', methods=['POST'])
 def pat_on_find():
     print("Patient find received!")
-    # print(request.json)
-    return render_template("index.html")
-    # return jsonify(summary = {"Patient Find Response": request.json})
+    print(request.json)
+    return jsonify(summary = {"Patient": "On Find"})
 
 @app.route('/v0.5/consent-requests/on-init', methods=['POST'])
 def con_req_on_init():
     print("Con Req on init received!")
-    # print(request.json)
-    return jsonify(summary = {"Con Req on init Response": request.json})
+    print(request.json)
+    return jsonify(summary = {"Con_Req": "On Init"})
 
 if __name__ == '__main__':
     app.run(debug=False)
